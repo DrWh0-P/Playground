a3c3d80bb6d13b3b8ba5f9b82f920df8574508fe
Paul <paulchrn@gmail.com>
2016-02-24 21:53:40 -0500

    Update follow.py

--- follow.py
+++ follow.py
@@ -5,13 +5,14 @@ import json
 
 
 def follow(url):
+    #substituting ? with.json or adding .json depending on the url
     if re.search("\\?",url):
         url=re.sub("\\?",".json?",url)
     else:
-        url=url + ".json"  
-    print(url)
+        url=url + ".json" 
     try:
         req=q.Request(url)
+        #to make sure the server doesnt identify as a bot
         req.add_header("User-Agent","Mozilla/5.0 (Windows; U; Windows NT 5.1; it; rv:1.8.1.11)  Gecko/20071127 Firefox/2.0.0.11")
         data=q.urlopen(req).read().decode()
         data=json.loads(data)
